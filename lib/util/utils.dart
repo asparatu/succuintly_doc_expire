aa5b05e86296830b1194436b5469dbc7d15368eb
asparatu <asparatu@gmail.com>
2020-05-24 09:32:25 +0800

    Added Validation Class

--- lib/util/utils.dart
+++ lib/util/utils.dart
@@ -1,1 +1,19 @@
+import 'dart:html';
 
+import 'package:intl/intl.dart';
+
+class Validation {
+  //Validation
+  static String validateTitle(String text) {
+    return (text != null && text != '') ? null : 'Title cannot be empty';
+  }
+
+  static String getExpiryStr(String expires) {
+    DateTime e = DateUtils.convertToDate(expires);
+    DateTime td = new DateTime.now();
+
+    Duration dif = e.difference(td);
+    int dd = dif.inDays + 1;
+    return (dd > 0) ? dd.toString() : '0';
+  }
+}
